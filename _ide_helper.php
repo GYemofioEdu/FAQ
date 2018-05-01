e6812e7bacddcd4d945be4760b6022dbd2e45b41
Godfred <gny2@njit.edu>
2018-05-01 02:14:25 -0400

    Update composer for Heroku

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.19 on 2018-05-01 02:28:54.
+ * Generated for Laravel 5.6.19 on 2018-05-01 06:11:44.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
