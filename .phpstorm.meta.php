c38b8f1c54afe310fe70e9ebd8aaba1217e968f5
Godfred <gny2@njit.edu>
2018-05-07 18:49:58 -0400

    Update composer for heroku

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-05-01 06:11:44.
+    * Generated on 2018-05-07 22:44:48.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
